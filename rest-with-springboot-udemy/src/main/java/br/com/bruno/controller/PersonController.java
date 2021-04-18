ccd786b5b10427ac39d879f97a36cf5888ebf3eb
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-18 12:31:09 -0300

    Customizando Swagger

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
@@ -18,50 +18,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bruno.data.vo.v1.PersonVO;
 import br.com.bruno.service.PersonServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-
+@Api(tags = "PersonEndpoint")
 @RestController
 @RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired
 	private PersonServices service;
-
+	
+	@ApiOperation(value = "Find all people" ) 
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
 	public List<PersonVO> findAll() {
-		List<PersonVO> persons = service.findAll();
-		persons.stream()
-				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		List<PersonVO> persons =  service.findAll();
+		persons
+			.stream()
+			.forEach(p -> p.add(
+					linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
+				)
+			);
 		return persons;
-	}
-
+	}	
+	
+	@ApiOperation(value = "Find a specific person by your ID" )
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
 	public PersonVO findById(@PathVariable("id") Long id) {
 		PersonVO personVO = service.findById(id);
 		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
 		return personVO;
-	}
-
-	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
-			"application/json", "application/xml", "application/x-yaml" })
+	}	
+	
+	@ApiOperation(value = "Create a new person") 
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
 	public PersonVO create(@RequestBody PersonVO person) {
 		PersonVO personVO = service.create(person);
 		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
 		return personVO;
 	}
-
-	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
-			"application/json", "application/xml", "application/x-yaml" })
+	
+	@ApiOperation(value = "Update a specific person")
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
 	public PersonVO update(@RequestBody PersonVO person) {
 		PersonVO personVO = service.update(person);
 		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
 		return personVO;
-	}
-
+	}	
+	
+	@ApiOperation(value = "Delete a specific person by your ID")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 		service.delete(id);
 		return ResponseEntity.ok().build();
-	}
+	}	
 
 }
