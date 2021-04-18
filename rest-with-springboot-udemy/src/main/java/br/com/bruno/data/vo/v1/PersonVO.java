c37628f2ef6690c2487c0e4701bc99552cfa843d
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-18 12:22:29 -0300

    Adicionando Swagger

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/data/vo/v1/PersonVO.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/data/vo/v1/PersonVO.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import org.springframework.hateoas.ResourceSupport;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.github.dozermapper.core.Mapping;
 
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
+@JsonPropertyOrder({ "id", "firstName", "lastName", "address", "gender" })
 public class PersonVO  extends ResourceSupport implements Serializable {
 
 	private static final long serialVersionUID = 1L;
