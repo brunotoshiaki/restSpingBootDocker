d87ce8578244f025083c9774a24f118c95bae28f
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-24 12:11:19 -0300

    Habilitanto cors

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import br.com.bruno.data.vo.v1.PersonVO;
 import br.com.bruno.service.PersonServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
+//@CrossOrigin
 @Api(tags = "PersonEndpoint")
 @RestController
 @RequestMapping("/api/person/v1")
@@ -42,6 +43,7 @@ public class PersonController {
 		return persons;
 	}	
 	
+	//@CrossOrigin(origins= "http://localhost:8080")
 	@ApiOperation(value = "Find a specific person by your ID" )
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
 	public PersonVO findById(@PathVariable("id") Long id) {
