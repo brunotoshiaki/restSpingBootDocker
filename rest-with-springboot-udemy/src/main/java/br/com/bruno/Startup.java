5dc833d282bd3cf8cca1e93e0271676b919f57fb
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-25 12:33:04 -0300

    Definindo Configuração

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/Startup.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/Startup.java
@@ -3,21 +3,24 @@ package br.com.bruno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.bruno.config.FileStorageConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageConfig.class })
 @EnableAutoConfiguration
 @ComponentScan
 public class Startup {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
-		
+    public static void main(String[] args) {
+	SpringApplication.run(Startup.class, args);
+
 //		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
 //		String result = bCryptPasswordEncoder.encode("admin123");
 //		System.out.println("My hash " + result);
-		
-	}
+
+    }
 
 }
