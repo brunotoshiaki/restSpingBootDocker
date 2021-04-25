34f0d6d1e39c0675bdcd7981b178c84a6215c86f
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-25 12:21:48 -0300

    Removendo warnings e adicionando pageable em book

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/BookController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/BookController.java
@@ -3,9 +3,15 @@ package br.com.bruno.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bruno.data.vo.v1.BookVO;
@@ -21,58 +28,66 @@ import br.com.bruno.service.BookServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(tags = "BookEndpoint") 
+@Api(tags = "BookEndpoint")
 @RestController
 @RequestMapping("/api/book/v1")
 public class BookController {
-	
-	@Autowired
-	private BookServices service;
-	
-	@ApiOperation(value = "Find all books" )
-	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public List<BookVO> findAll() {
-		List<BookVO> books =  service.findAll();
-		books
-			.stream()
-			.forEach(p -> p.add(
-					linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()
-				)
-			);
-		return books;
-	}	
-	
-	@ApiOperation(value = "Find a specific book by your ID" )
-	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
-	public BookVO findById(@PathVariable("id") Long id) {
-		BookVO bookVO = service.findById(id);
-		bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
-		return bookVO;
-	}	
-	
-	@ApiOperation(value = "Create a new book")
-	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
-			consumes = { "application/json", "application/xml", "application/x-yaml" })
-	public BookVO create(@RequestBody BookVO book) {
-		BookVO bookVO = service.create(book);
-		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
-		return bookVO;
-	}
-	
-	@ApiOperation(value = "Update a specific book")
-	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
-			consumes = { "application/json", "application/xml", "application/x-yaml" })
-	public BookVO update(@RequestBody BookVO book) {
-		BookVO bookVO = service.update(book);
-		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
-		return bookVO;
-	}	
-	
-	@ApiOperation(value = "Delete a specific book by your ID")
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-		service.delete(id);
-		return ResponseEntity.ok().build();
-	}	
-	
+
+    @Autowired
+    private BookServices service;
+
+    @Autowired
+    private PagedResourcesAssembler<BookVO> assembler;
+
+    @ApiOperation(value = "Find all books")
+    @GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+    public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+	    @RequestParam(value = "limit", defaultValue = "12") int limit,
+	    @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+	var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+
+	Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "title"));
+
+	Page<BookVO> books = service.findAll(pageable);
+	books.stream().forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+
+	PagedResources<?> resources = assembler.toResource(books);
+
+	return new ResponseEntity<>(resources, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Find a specific book by your ID")
+    @GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+    public BookVO findById(@PathVariable("id") Long id) {
+	BookVO bookVO = service.findById(id);
+	bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+	return bookVO;
+    }
+
+    @ApiOperation(value = "Create a new book")
+    @PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+	    "application/json", "application/xml", "application/x-yaml" })
+    public BookVO create(@RequestBody BookVO book) {
+	BookVO bookVO = service.create(book);
+	bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+	return bookVO;
+    }
+
+    @ApiOperation(value = "Update a specific book")
+    @PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+	    "application/json", "application/xml", "application/x-yaml" })
+    public BookVO update(@RequestBody BookVO book) {
+	BookVO bookVO = service.update(book);
+	bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+	return bookVO;
+    }
+
+    @ApiOperation(value = "Delete a specific book by your ID")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+	service.delete(id);
+	return ResponseEntity.ok().build();
+    }
+
 }
--- rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
@@ -38,19 +38,23 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
+    @Autowired
+    private PagedResourcesAssembler<PersonVO> assembler;
+
     @ApiOperation(value = "Find all people")
     @GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-    public ResponseEntity<PagedResources<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+    public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
 	    @RequestParam(value = "limit", defaultValue = "12") int limit,
-	    @RequestParam(value = "direction", defaultValue = "asc") String direction,
-	    PagedResourcesAssembler assembler) {
+	    @RequestParam(value = "direction", defaultValue = "asc") String direction) {
 	var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 
 	Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
 	Page<PersonVO> persons = service.findAll(pageable);
 	persons.stream()
 		.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-	return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
+	PagedResources<?> resources = assembler.toResource(persons);
+
+	return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Find a specific person by name")
@@ -59,8 +63,7 @@ public class PersonController {
     public ResponseEntity<?> findPersonByName(@PathVariable("firstName") String firstName,
 	    @RequestParam(value = "page", defaultValue = "0") int page,
 	    @RequestParam(value = "limit", defaultValue = "12") int limit,
-	    @RequestParam(value = "direction", defaultValue = "asc") String direction,
-	    PagedResourcesAssembler assembler) {
+	    @RequestParam(value = "direction", defaultValue = "asc") String direction) {
 
 	var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 
