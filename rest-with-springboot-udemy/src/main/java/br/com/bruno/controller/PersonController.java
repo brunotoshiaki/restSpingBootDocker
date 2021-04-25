6aa8749999a2ca7e291f741e243275f2273b9b16
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-04-25 11:44:22 -0300

    Adicionando paginação com pageable e query parmas

--- rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/bruno/controller/PersonController.java
@@ -6,6 +6,8 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,74 +17,74 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bruno.data.vo.v1.PersonVO;
 import br.com.bruno.service.PersonServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-//@CrossOrigin
+
+// @CrossOrigin
 @Api(tags = "PersonEndpoint")
 @RestController
 @RequestMapping("/api/person/v1")
 public class PersonController {
 
-	@Autowired
-	private PersonServices service;
-	
-	@ApiOperation(value = "Find all people" ) 
-	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public List<PersonVO> findAll() {
-		List<PersonVO> persons =  service.findAll();
-		persons
-			.stream()
-			.forEach(p -> p.add(
-					linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
-				)
-			);
-		return persons;
-	}	
-	
-	//@CrossOrigin(origins= "http://localhost:8080")
-	@ApiOperation(value = "Find a specific person by your ID" )
-	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
-	public PersonVO findById(@PathVariable("id") Long id) {
-		PersonVO personVO = service.findById(id);
-		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
-		return personVO;
-	}	
-	
-	@ApiOperation(value = "Create a new person") 
-	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
-			consumes = { "application/json", "application/xml", "application/x-yaml" })
-	public PersonVO create(@RequestBody PersonVO person) {
-		PersonVO personVO = service.create(person);
-		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
-		return personVO;
-	}
-	
-	@ApiOperation(value = "Update a specific person")
-	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
-			consumes = { "application/json", "application/xml", "application/x-yaml" })
-	public PersonVO update(@RequestBody PersonVO person) {
-		PersonVO personVO = service.update(person);
-		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
-		return personVO;
-	}	
-	
-	@ApiOperation(value = "Delete a specific person by your ID")
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-		service.delete(id);
-		return ResponseEntity.ok().build();
-	}	
-	
-	@ApiOperation(value = "Disable a specific person by your ID" )
-	@PatchMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
-	public PersonVO disablePerson(@PathVariable("id") Long id) {
-		PersonVO personVO = service.disablePerson(id);
-		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
-		return personVO;
-	}	
+    @Autowired
+    private PersonServices service;
+
+    @ApiOperation(value = "Find all people")
+    @GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+    public List<PersonVO> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+	    @RequestParam(value = "limit", defaultValue = "12") int limit) {
+	Pageable pageable = PageRequest.of(page, limit);
+	List<PersonVO> persons = service.findAll(pageable);
+	persons.stream()
+		.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+	return persons;
+    }
+
+    // @CrossOrigin(origins= "http://localhost:8080")
+    @ApiOperation(value = "Find a specific person by your ID")
+    @GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+    public PersonVO findById(@PathVariable("id") Long id) {
+	PersonVO personVO = service.findById(id);
+	personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+	return personVO;
+    }
+
+    @ApiOperation(value = "Create a new person")
+    @PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+	    "application/json", "application/xml", "application/x-yaml" })
+    public PersonVO create(@RequestBody PersonVO person) {
+	PersonVO personVO = service.create(person);
+	personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+	return personVO;
+    }
+
+    @ApiOperation(value = "Update a specific person")
+    @PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+	    "application/json", "application/xml", "application/x-yaml" })
+    public PersonVO update(@RequestBody PersonVO person) {
+	PersonVO personVO = service.update(person);
+	personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+	return personVO;
+    }
+
+    @ApiOperation(value = "Delete a specific person by your ID")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+	service.delete(id);
+	return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation(value = "Disable a specific person by your ID")
+    @PatchMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+    public PersonVO disablePerson(@PathVariable("id") Long id) {
+	PersonVO personVO = service.disablePerson(id);
+	personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+	return personVO;
+    }
 
 }
